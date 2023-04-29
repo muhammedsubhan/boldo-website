78acc79c26ec90c4c169cf6361aa4ee53f5a3da8
Subhan <muhammedsubhan692@gmail.com>
2023-04-30 00:54:29 +0500

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../icons/Logo.png";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex p-5 justify-between items-center">
+        <div className="flex space-x-5 ml-14">
+          <img src={logo} alt="logo" />
+          <h1 className="text-blue-950 font-bold text-4xl">BOLDO</h1>
+        </div>
+
+        <div className="flex justify-end items-center pr-16 space-x-14 text-blue-950 font-semibold text-base">
+          <p>Product</p>
+          <p>Services</p>
+          <p>About</p>
+          <button className="px-8 py-2 border-2 rounded-full hover:border-blue-950  text-white bg-blue-950 hover:bg-white hover:text-blue-950 transition-all ease-in-out">
+            Log In
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
