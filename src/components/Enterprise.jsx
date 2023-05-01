daaf7a5d6108633911f2c002d7b498b0571c6c15
Subhan <muhammedsubhan692@gmail.com>
2023-05-02 02:42:19 +0500

    Slidebar animation

--- src/components/Enterprise.jsx
+++ src/components/Enterprise.jsx
@@ -3,9 +3,37 @@ import Comments from "../pages/Comments";
 import comment1 from "../icons/comment1.png";
 import comment2 from "../icons/comment2.png";
 import comment3 from "../icons/comment3.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Enterprise = () => {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   const data = [
+    {
+      title:
+        "“Buyer buzz partner network disruptive non-disclosure agreement business”",
+      img: comment1,
+      name: "Albus Dumbledore",
+      m: "Manager @ Howarts",
+    },
+
+    {
+      title:
+        "“Buyer buzz partner network disruptive non-disclosure agreement business”",
+      img: comment1,
+      name: "Albus Dumbledore",
+      m: "Manager @ Howarts",
+    },
+
     {
       title:
         "“Buyer buzz partner network disruptive non-disclosure agreement business”",
@@ -20,6 +48,21 @@ const Enterprise = () => {
       name: "Severus Snape",
       m: "Manager @ Slytherin",
     },
+
+    {
+      title:
+        "“Learning curve infrastructure value proposition advisor strategy user experience hypotheses investor.”",
+      img: comment2,
+      name: "Severus Snape",
+      m: "Manager @ Slytherin",
+    },
+    {
+      title:
+        "“Release facebook responsive web design business model canvas seed money monetization.”",
+      img: comment3,
+      name: "Harry Potter",
+      m: "Team Leader @ Gryffindor",
+    },
     {
       title:
         "“Release facebook responsive web design business model canvas seed money monetization.”",
@@ -29,28 +72,57 @@ const Enterprise = () => {
     },
   ];
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   return (
     <>
       <div className="p-20 bg-blue-950">
         <div className="w-2/4  ml-16">
-          <h1 className="leading-normal font-normal text-5xl text-white px-8  ">
+          <h1 className="leading-normal font-normal text-5xl text-white  ">
             An enterprise template to ramp up your company website
           </h1>
         </div>
         <div className="flex justify-end items-center gap-12 ">
-          <button className="h-16 w-16 bg-white rounded-full">
+          <button
+            className="h-16 w-16 bg-white rounded-full"
+            onClick={slideLeft}
+          >
             <i class="fa-solid fa-arrow-left text-xl"></i>
           </button>
-          <button className="h-16 w-16 bg-white rounded-full">
+          <button
+            className="h-16 w-16 bg-white rounded-full"
+            onClick={slideRight}
+          >
             <i class="fa-solid fa-arrow-right text-xl"></i>
           </button>
         </div>
 
-        <div className="flex justify-evenly items-center">
-          {data.map((item, index) => {
-            return <Comments key={index} data={item} />;
-          })}
-        </div>
+        <Carousel responsive={responsive}>
+          <div
+            id="slider"
+            className="flex justify-evenly items-center overflow-x-hidden scroll-smooth infinite gap-12 w-screen"
+          >
+            {data.map((item, index) => {
+              return <Comments key={index} data={item} />;
+            })}
+          </div>
+        </Carousel>
       </div>
     </>
   );
--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -4,7 +4,7 @@ const Comments = ({ data }) => {
   return (
     <>
       <div>
-        <div className="h-96 w-96 bg-white p-12 rounded-xl mt-10 ">
+        <div className="h-96 w-96 bg-white p-12 rounded-xl mt-10">
           <h2 className="font-normal text-2xl mb-2">{data.title}</h2>
           <div className="flex items-center mt-10 gap-4">
             <img src={data.img} alt="" />
